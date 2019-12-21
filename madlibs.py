c17e6e789bd8b98532de314f8b6c22a75a4ebf43
Buhamad <mr.buhamad@hotmail.com>
2019-12-21 03:05:43 +0300

    debug story script

--- madlibs.py
+++ madlibs.py
@@ -6,9 +6,9 @@ scream= input ("Enter any sentence  ")
 	# command has to be .upper()#
 action= input ("Enter a verb  ")
 
-Story = """ The story goes...
+story = """ The story goes...
 
-It was %d o'clock when I heard a knock at the door.
+It was %s o'clock when I heard a knock at the door.
 I opened the door and there was a box full of %s with a note saying "From Mr. %s."
 Just as I closed the door I heard a scream "%s"
 I froze in place and all I could do was %s """ % (time, items,name.capitalize(),scream.upper(),action)
