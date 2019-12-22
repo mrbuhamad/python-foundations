431b03f1286334f366def8b8292f74378b1f36d1
Buhamad <mr.buhamad@hotmail.com>
2019-12-22 19:37:02 +0300

    add calculater file

---
+++ calculator.py
@@ -0,0 +1,13 @@
+1stnumber= input int(("Enter the first number  "))
+2ndnumber= input int(("Enter the second number  "))
+opration= input ("Choose the operation (+, -, /, *)  ")
+if opration== "+":
+    print 1stnumber + 2ndnumber
+elif opration== "-":
+	print 1stnumber - 2ndnumber
+elif opration== "/":
+	print 1stnumber / 2ndnumber
+elif opration== "*":
+	print 1stnumber * 2ndnumber
+else:
+	print "Choose the operation (+, -, /, *)"
